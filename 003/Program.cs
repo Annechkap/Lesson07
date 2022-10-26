d719e1917e0ca8efe4280d37da3f9c623a88e7a0
Anna <annechkap@list.ru>
2022-10-26 20:42:37 +0300

    Добавлена задача 003

---
+++ 003/Program.cs
@@ -0,0 +1,49 @@
+﻿// 3. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] CreateArray(int lenRows, int lenColumns)
+{
+    int[,] array = new int[lenRows, lenColumns];
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(1, 50);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] array = CreateArray(3, 4);
+PrintArray(array);
+
+int rows = array.GetLength(0);
+
+void avarageNum(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double avarage = 0;
+        for (int i = 0; i < rows; i++)
+        {
+            avarage = (avarage + array[i, j]);
+        }
+        avarage = avarage / rows;
+        Console.Write($"{avarage:f2}\t");
+    }
+}
+
+avarageNum(array);
+Console.WriteLine();
