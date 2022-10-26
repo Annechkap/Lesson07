5e11306a8cb08193d0df5c42a4958d6b0cfbf88e
Anna <annechkap@list.ru>
2022-10-26 20:41:41 +0300

    Добавлена задача 001

---
+++ 001/Program.cs
@@ -0,0 +1,30 @@
+﻿// 1. Задайте двумерный массив размером mxn, заполните случайными вещественными числами
+
+double[,] CreateArray(int lenRows, int lenColumns)
+{
+    double[,] array = new double[lenRows, lenColumns];
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(1, 60) / 10;
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]:f2}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+double[,] array = CreateArray(3, 4);
+PrintArray(array);
